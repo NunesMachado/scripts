8ef3da2e7087eb6d7e7db67dfd7792c06c14d4c9
Roberson Nunes Machado <roberson.nunes@totvs.com.br>
2021-09-06 11:08:04 -0300

    feature: Adicionado troca de settings, e corrigido caminhos do maven

--- simpleEnv/src/br/com/ws/simpleEnv/Service.java
+++ simpleEnv/src/br/com/ws/simpleEnv/Service.java
@@ -14,9 +14,13 @@ import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Service {
 
+	private static final String WINDOWS_OS = "Windows";
+	private static final String JA_EXISTE = " já existe, por isso não será criado!";
+	private static final String SIMPLE_ENV_LOG = "> [SIMPLE_ENV] ";
 	private static final String CAMINHO_WORKSPACE_STANDARD = "CAMINHO_WORKSPACE_STANDARD";
 	private static final String CAMINHO_MAVEN_SETTINGS = "CAMINHO_MAVEN_SETTINGS";
 	private static final String CAMINHO_FONTES = "CAMINHO_FONTES";
@@ -27,6 +31,12 @@ public class Service {
 	private static final String SIM3G = "sim3g";
 
 	private static final String LOCAL_REPOSITORY = "localRepository";
+	private static final String CAMINHO_LIB_MAVEN_CONFIG = "CAMINHO_LIB_MAVEN_CONFIG";
+	private static final String SVN = "SVN";
+	private static final String GIT = "GIT";
+	private static final String GIT_SETTINGS = "settings_git.xml";
+	private static final String SVN_SETTINGS = "settings_svn.xml";
+	private static final String CONFIG = "config.properties";
 
 	private Properties props;
 
@@ -36,40 +46,54 @@ public class Service {
 
 	private static Properties getProperties() throws FileNotFoundException, IOException {
 		Properties props = new Properties();
-		InputStream is = new FileInputStream("config.properties");
+		File config = new File(CONFIG);
+		InputStream is = new FileInputStream(config);
 		props.load(is);
 		return props;
 	}
 
 	private void constroiAmbiente() throws FileNotFoundException, IOException {
+		System.out.println("Construindo ambiente!");
 		File diretorio = new File(props.getProperty(CAMINHO_FONTES));
 		File[] listFiles = diretorio.listFiles();
 		for (int i = 0; i < listFiles.length; i++) {
 			File file = listFiles[i];
 			if (file.isDirectory()) {
-				criaDiretorio(file.getAbsolutePath(), SIM3G);
+				log("|-------------------------------------------------------------------------------|");
 				criaDiretorioWorkspace(file);
+				criaDiretorio(file.getAbsolutePath(), SIM3G);
 				copiaArquivoMaven(criaDiretorio(file.getAbsolutePath(), MAVEN));
+				log("|-------------------------------------------------------------------------------|");
 			}
 		}
+		log("Ambiente construido!");
 	}
 
 	private void criaDiretorioWorkspace(File file) throws IOException {
+		log("Criando workspace " + file);
 		File workspace = new File(file.getAbsolutePath(), WORKSPACE);
-		if (!workspace.exists())
+		if (!workspace.exists()) {
+			log("Workspace criado !");
 			copyDirectory(new File(props.getProperty(CAMINHO_WORKSPACE_STANDARD)), workspace);
+		} else {
+			log(WORKSPACE + JA_EXISTE);
+		}
 	}
 
 	private File criaDiretorio(String caminho, String nome) {
+		log("Criando diretorio " + nome);
 		File file = new File(caminho, nome);
 		if (!file.exists()) {
 			file.mkdir();
+			log("Diretório criado: " + nome);
+		} else {
+			log(nome + JA_EXISTE);
 		}
 		return file;
 	}
 
-	private void copiaArquivoMaven(File maven)
-			throws FileNotFoundException, IOException {
+	private void copiaArquivoMaven(File maven) throws FileNotFoundException, IOException {
+		log("Copiando arquivo maven settings " + maven);
 		File settings = new File(maven.getAbsolutePath(), SETTINGS_XML);
 
 		if (!settings.exists()) {
@@ -85,7 +109,13 @@ public class Service {
 			String linha = br.readLine();
 			while (linha != null) {
 				if (linha.contains(LOCAL_REPOSITORY)) {
-					linha = linha.replaceAll("\\>(.*)\\<", ">" + maven.getAbsolutePath() + "<");
+					String mavenPath;
+					if(isWindows()) {
+						mavenPath = maven.getAbsolutePath().replace("\\", "\\\\");
+					} else {
+						mavenPath = maven.getAbsolutePath();
+					}					
+					linha = linha.replaceAll("\\>(.*)\\<", ">" + mavenPath + "<");
 				}
 				bw.write(linha);
 				bw.newLine();
@@ -94,6 +124,8 @@ public class Service {
 			}
 			br.close();
 			bw.close();
+		} else {
+			log("Settings já existe! por isso não será copiado!");
 		}
 	}
 
@@ -122,13 +154,68 @@ public class Service {
 		in.close();
 		out.close();
 	}
-	
-	public static void main(String[] args) throws IOException {
 
+	public static void main(String[] args) throws IOException {
 		Properties props = getProperties();
 		Service servico = new Service(props);
-		servico.constroiAmbiente();
+		boolean stillRunning= true;	
+		Scanner scan = new Scanner(System.in);
+		while (stillRunning) {
 
+			log("Deseja criar um novo ambiente de configuração para o SVN  ou alterar config do settings? AMBIENTE(A), SETTINGS(S) ou EXIT para sair.");
+			String input = scan.next();
+		
+			if (input.equalsIgnoreCase("exit")) {				
+				scan.close();
+				stillRunning = false;
+			}
+			if (input.toUpperCase().equals("A")) {
+				servico.constroiAmbiente();
+			} else if(input.toUpperCase().equals("S")) {
+				servico.alteraSettings(scan);
+			} else {
+				stillRunning = false;
+				log("Input informado não encontrado!");
+			}
+		}
+		
+		scan.close();
 	}
 
+	private void alteraSettings(Scanner scan) throws IOException {
+		log("Alterar para SVN ou para GIT ?");
+		String input = scan.next();
+
+		if(isSVN(input)){
+			File settingsSVN = new File(SVN_SETTINGS);
+			copy(settingsSVN, getCaminhoSettingMaven());
+			log("alterado para settings SVN");
+
+		} else if(isGIT(input)) {
+			File settingsGit = new File(GIT_SETTINGS);
+			copy(settingsGit, getCaminhoSettingMaven());
+			log("alterado para settings GIT");
+		} else {
+			log("Input informado não existe!");
+		}
+	}
+
+	private boolean isGIT(String input) {
+		return input.toUpperCase().equals(GIT);
+	}
+
+	private boolean isSVN(String input) {
+		return input.toUpperCase().equals(SVN);
+	}
+
+	private File getCaminhoSettingMaven() {
+		return new File(props.getProperty(CAMINHO_LIB_MAVEN_CONFIG), SETTINGS_XML);
+	}
+
+	private static void log(String msg) {
+		System.out.println(SIMPLE_ENV_LOG + msg);
+	}
+	private static boolean isWindows() {
+		return System.getProperty("os.name").contains(WINDOWS_OS);
+	}
 }
